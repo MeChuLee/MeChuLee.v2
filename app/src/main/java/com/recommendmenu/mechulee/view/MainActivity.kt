124f0151ad5f50f059d8f4ca696c2927db285d10
JungWooGeon <jwk1734@naver.com>
2023-08-13 15:59:05 +0900

    bottom bar 숨기기 기능 추가

--- app/src/main/java/com/recommendmenu/mechulee/view/MainActivity.kt
+++ app/src/main/java/com/recommendmenu/mechulee/view/MainActivity.kt
@@ -11,22 +11,53 @@ import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNa
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val BOTTOM_BAR_STATUS_SHOW = 0
+        const val BOTTOM_BAR_STATUS_HIDE = 1
+    }
+
     private lateinit var binding: ActivityMainBinding
+    lateinit var mainActivityListener: MainActivityListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         initBottomNavigationBar()
+        initListener()
 
         // StatusBar 색 변경
         window.statusBarColor = ContextCompat.getColor(this, R.color.white)
     }
 
+    // ExpandableBottomBar 에 Navigation 설정
     private fun initBottomNavigationBar() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        ExpandableBottomBarNavigationUI.setupWithNavController(binding.expandableBottomBar, navController)
+        ExpandableBottomBarNavigationUI.setupWithNavController(
+            binding.expandableBottomBar,
+            navController
+        )
+    }
+
+    // Listener 설정
+    private fun initListener() {
+        mainActivityListener = object : MainActivityListener {
+            override fun changeBottomBarStatus(visibility: Int) {
+                // 변경할 상태에 따라 bottom bar show/hide
+                when (visibility) {
+                    BOTTOM_BAR_STATUS_SHOW -> binding.expandableBottomBar.show()
+                    BOTTOM_BAR_STATUS_HIDE -> binding.expandableBottomBar.hide()
+                }
+            }
+        }
+    }
+
+    // Child Fragment 들에서 BottomBar 를 Control 하기 위해 MainActivity 와 통신하는 Listener
+    interface MainActivityListener {
+        // visibility : BOTTOM_BAR_STATUS_SHOW, BOTTOM_BAR_STATUS_HIDE 으로 설정하기
+        fun changeBottomBarStatus(visibility: Int)
     }
 }
--- app/src/main/java/com/recommendmenu/mechulee/view/menu_list/MenuListFragment.kt
+++ app/src/main/java/com/recommendmenu/mechulee/view/menu_list/MenuListFragment.kt
@@ -14,7 +14,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.recommendmenu.mechulee.databinding.FragmentMenuListBinding
+import com.recommendmenu.mechulee.view.MainActivity
 import com.recommendmenu.mechulee.view.menu_list.adapter.MenuCategoryAdapter
 import com.recommendmenu.mechulee.view.menu_list.adapter.MenuGridAdapter
 import com.recommendmenu.mechulee.view.menu_list.adapter.MenuListAdapter
@@ -32,9 +34,11 @@ class MenuListFragment : Fragment() {
 
     private lateinit var viewModel: MenuListViewModel
 
+    // 메뉴 리스트 RecyclerView 에서 사용할 두 가지 Adapter (List, Grid)
     private lateinit var menuListRecyclerViewAdapter: MenuListAdapter
     private lateinit var menuGridRecyclerViewAdapter: MenuGridAdapter
 
+    // 현재 RecyclerView 에서 보여주고 있는 방식 상태 (MENU_TYPE_LIST, MENU_TYPE_GRID)
     private var currentMenuType = MENU_TYPE_LIST
 
     @SuppressLint("NotifyDataSetChanged")
@@ -46,8 +50,10 @@ class MenuListFragment : Fragment() {
         _binding = FragmentMenuListBinding.inflate(layoutInflater)
 
         viewModel = ViewModelProvider(this)[MenuListViewModel::class.java]
+        // viewModel 을 선언한 후, initRecyclerView() 를 순서대로 실행해야 동기적 선언 오류가 없음
         initRecyclerView()
 
+        // 메뉴 리스트 observe -> 변경 감지 시 메뉴 정보 리스트 RecyclerView 에 반영
         viewModel.menuList.observe(requireActivity()) { menuList ->
             menuListRecyclerViewAdapter.list = menuList
             menuGridRecyclerViewAdapter.list = menuList
@@ -68,6 +74,7 @@ class MenuListFragment : Fragment() {
         // 메뉴 카테고리 RecyclerView 초기화
         val menuCategoryRecyclerViewAdapter = MenuCategoryAdapter()
 
+        // viewModel 에 선언되어 있는 categoryList 를 추가 (고정된 값이기 때문에 바로 반영, observe 사용 x)
         viewModel.categoryList.value?.forEach { menuCategoryRecyclerViewAdapter.list.add(it) }
 
         binding.menuCategoryRecyclerView.apply {
@@ -76,6 +83,7 @@ class MenuListFragment : Fragment() {
             adapter = menuCategoryRecyclerViewAdapter
         }
 
+        // 메뉴 리스트 RecyclerView 초기화
         menuListRecyclerViewAdapter = MenuListAdapter()
         menuGridRecyclerViewAdapter = MenuGridAdapter()
 
@@ -83,11 +91,26 @@ class MenuListFragment : Fragment() {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             adapter = menuListRecyclerViewAdapter
+
+            // Scroll 이벤트 추가 -> MainActivity Bottom Bar 를 컨트롤하기 위해 MainActivity 에 Listener 로 알림
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    if (dy > 0) {
+                        // 아래로 스크롤 시 Bottom Bar 사라짐
+                        (activity as? MainActivity)?.mainActivityListener?.changeBottomBarStatus(MainActivity.BOTTOM_BAR_STATUS_HIDE)
+                    } else {
+                        // 위로 스크롤 시 Bottom Bar 나타남
+                        (activity as? MainActivity)?.mainActivityListener?.changeBottomBarStatus(MainActivity.BOTTOM_BAR_STATUS_SHOW)
+                    }
+                }
+            })
         }
     }
 
     private fun initButton() {
-        // '리스트' 아이콘 클릭 시 -> Fragment 전환
+        // '리스트' 아이콘 클릭 시 -> 메뉴 리스트 RecyclerView Adapter 전환 (List 형식)
         binding.menuListImageIcon.setOnClickListener {
             binding.menuListRecyclerView.apply {
                 layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
@@ -95,7 +118,7 @@ class MenuListFragment : Fragment() {
             }
         }
 
-        // '그리드' 아이콘 클릭 시 -> Fragment 전환
+        // '그리드' 아이콘 클릭 시 -> 메뉴 리스트 RecyclerView Adapter 전환 (Grid 형식)
         binding.menuListGridIcon.setOnClickListener {
             binding.menuListRecyclerView.apply {
                 layoutManager = GridLayoutManager(requireContext(), GRID_LAYOUT_SPAN_COUNT)
