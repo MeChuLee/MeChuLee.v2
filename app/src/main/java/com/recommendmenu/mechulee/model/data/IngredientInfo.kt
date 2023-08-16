94b913d7144e5dedf90efd08e2ebdd6c29a857df
yountaeseop <jangmj6931@gmail.com>
2023-08-17 00:10:26 +0900

    주석 추가

--- app/src/main/java/com/recommendmenu/mechulee/model/data/IngredientInfo.kt
+++ app/src/main/java/com/recommendmenu/mechulee/model/data/IngredientInfo.kt
@@ -3,3 +3,4 @@ package com.recommendmenu.mechulee.model.data
 // Model - RecyclerView에 들어가는 데이터 모델
 // image - text - rating 순서
 data class IngredientInfo(val imageResId: Int, val title: String, var rating: Float)
+// -> val classification: String으로 분류 속성 추가해서 코드 수정하기,
