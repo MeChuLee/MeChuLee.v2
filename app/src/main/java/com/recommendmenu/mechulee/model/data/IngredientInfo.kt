60f1f27b68bf47b75370e554c5d7e606907793d5
yountaeseop <jangmj6931@gmail.com>
2023-08-21 18:25:00 +0900

    오류 수정

--- app/src/main/java/com/recommendmenu/mechulee/model/data/IngredientInfo.kt
+++ app/src/main/java/com/recommendmenu/mechulee/model/data/IngredientInfo.kt
@@ -3,4 +3,4 @@ package com.recommendmenu.mechulee.model.data
 // Model - RecyclerView에 들어가는 데이터 모델
 
 // id - image - text - rating - classification 순서
-data class IngredientInfo(val ingredientId: Int, val imageResId: Int, val title: String, var rating: Float, var classification: String)
+data class IngredientInfo(val imageResId: Int, val title: String, var rating: Float, var classification: String)
