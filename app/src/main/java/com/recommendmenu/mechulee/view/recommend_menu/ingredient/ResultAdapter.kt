8c5602a85e300bbf787002b323696522324dde65
SeoJeongchan <wjdcks822@gmail.com>
2023-08-21 20:13:37 +0900

    결과 화면 추가

---
+++ app/src/main/java/com/recommendmenu/mechulee/view/recommend_menu/ingredient/ResultAdapter.kt
@@ -0,0 +1,34 @@
+package com.recommendmenu.mechulee.view.recommend_menu.ingredient
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.recommendmenu.mechulee.R
+
+class ResultAdapter: RecyclerView.Adapter<ResultAdapter.ViewHolder>() {
+    var datas = ArrayList<String>()
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val otherMenuImage: AppCompatImageView = itemView.findViewById(R.id.resultImageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_view_ingredient_result, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val myExample = R.drawable.example_result
+        holder.otherMenuImage.setImageResource(myExample)
+    }
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+
+}
