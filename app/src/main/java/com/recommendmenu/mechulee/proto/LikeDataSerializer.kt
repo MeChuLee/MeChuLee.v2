5c9f0073e190aa2021967bfeede448f194424f7e
SeoJeongchan <wjdcks822@gmail.com>
2023-09-06 23:15:14 +0900

    결과화면 DataStore 추가

---
+++ app/src/main/java/com/recommendmenu/mechulee/proto/LikeDataSerializer.kt
@@ -0,0 +1,34 @@
+package com.recommendmenu.mechulee.proto
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.google.protobuf.InvalidProtocolBufferException
+import com.recommendmenu.mechulee.IngredientData
+import com.recommendmenu.mechulee.LikeData
+import java.io.InputStream
+import java.io.OutputStream
+
+object LikeDataSerializer : Serializer<LikeData> {
+    override val defaultValue: LikeData = LikeData.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): LikeData {
+        try {
+            return LikeData.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(
+        t: LikeData,
+        output: OutputStream
+    ) = t.writeTo(output)
+}
+
+val Context.likedMenuDataStore: DataStore<LikeData> by dataStore(
+    fileName = "likeData.pb",
+    serializer = LikeDataSerializer
+)
