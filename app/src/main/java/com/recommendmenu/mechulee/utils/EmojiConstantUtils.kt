f797bd21b73cbdc3a8ddb3983382098d2d84d3e1
yountaeseop <jangmj6931@gmail.com>
2023-09-13 16:42:52 +0900

    EmojiConstants 음식 관련 이모지로 변경 + utils로 파일 이동

---
+++ app/src/main/java/com/recommendmenu/mechulee/utils/EmojiConstantUtils.kt
@@ -0,0 +1,136 @@
+package com.recommendmenu.mechulee.utils
+
+object EmojiConstantUtils {
+    var emojiCodePoints = intArrayOf( // 총 120개
+        // Supplemental Symbols And Pictographs - Food symbols - 44
+        0x1F950,  // CROISSANT
+        0x1F951,  // AVOCADO
+        0x1F952,  // CUCUMBER
+        0x1F953,  // BACON
+        0x1F954,  // POTATO
+        0x1F955,  // CARROT
+        0x1F956,  // BAGUETTE BREAD
+        0x1F957,  // GREEN SALAD
+        0x1F958,  // SHALLOW PAN OF FOOD
+        0x1F959,  // STUFFED FLATBREAD
+        0x1F95A,  // EGG
+        0x1F95B,  // GLASS OF MILK
+        0x1F95C,  // PEANUTS
+        0x1F95D,  // KIWIFRUIT
+        0x1F95E,  // PANCAKES
+        0x1F95F,  // DUMPLING
+        0x1F960,  // FORTUNE COOKIE
+        0x1F961,  // TAKEOUT BOX
+        0x1F962,  // CHOPSTICKS
+        0x1F963,  // BOWL WITH SPOON
+        0x1F964,  // CUP WITH STRAW
+        0x1F965,  // COCONUT
+        0x1F966,  // BROCCOLI
+        0x1F967,  // PIE
+        0x1F968,  // PRETZEL
+        0x1F969,  // CUT OF MEAT
+        0x1F96A,  // SANDWICH
+        0x1F96B,  // CANNED FOOD
+        0x1F96C,  // LEAFY GREEN
+        0x1F96D,  // MANGO
+        0x1F96E,  // MOON CAKE
+        0x1F96F,  // BAGEL
+        0x1F9C0,  // CHEESE WEDGE
+        0x1F9C1,  // CUPCAKE
+        0x1F9C2,  // SALT SHAKER
+        0x1F9C3,  // BEVERAGE BOX
+        0x1F9C4,  // GARLIC
+        0x1F9C5,  // ONION
+        0x1F9C6,  // FALAFEL
+        0x1F9C7,  // WAFFLE
+        0x1F9C8,  // BUTTER
+        0x1F9C9,  // MATE DRINK
+        0x1F9CA,  // ICE CUBE
+        0x1F9CB,  // BUBBLE TEA
+
+        // Miscellaneous Symbols And Pictographs - Plant symbols - 20
+        0x1F330,  // CHESTNUT
+        0x1F331,  // SEEDLING
+        //0x1F332,  // EVERGREEN TREE
+        0x1F333,  // DECIDUOUS TREE
+        0x1F334,  // PALM TREE
+        0x1F335,  // CACTUS
+        0x1F336,  // HOT PEPPER
+        0x1F337,  // TULIP
+        0x1F338,  // CHERRY BLOSSOM
+        0x1F339,  // ROSE
+        0x1F33A,  // HIBISCUS
+        0x1F33B,  // SUNFLOWER
+        0x1F33C,  // BLOSSOM
+        0x1F33D,  // EAR OF MAIZE
+        0x1F33E,  // EAR OF RICE
+        0x1F33F,  // HERB
+        0x1F340,  // FOUR LEAF CLOVER
+        0x1F341,  // MAPLE LEAF
+        0x1F342,  // FALLEN LEAF
+        0x1F343,  // LEAF FLUTTERING IN WIND
+        0x1F344,  // MUSHROOM
+
+        // Miscellaneous Symbols And Pictographs - Fruit and vegetable symbols - 15
+        0x1F345,  // TOMATO
+        0x1F346,  // AUBERGINE
+        0x1F347,  // GRAPES
+        0x1F348,  // MELON
+        0x1F349,  // WATERMELON
+        0x1F34A,  // TANGERINE
+        0x1F34B,  // LEMON
+        0x1F34C,  // BANANA
+        0x1F34D,  // PINEAPPLE
+        0x1F34E,  // RED APPLE
+        0x1F34F,  // GREEN APPLE
+        0x1F340,  // PEAR
+        0x1F351,  // PEACH
+        0x1F352,  // CHERRIES
+        0x1F353,  // STRAWBERRY
+
+        // Miscellaneous Symbols And Pictographs - Food symbols - 37
+        0x1F32D,  // HOT DOG
+        0x1F32E,  // TACO
+        0x1F32F,  // BURRITO
+        0x1F354,  // HAMBURGER
+        0x1F355,  // SLICE OF PIZZA
+        0x1F356,  // MEAT ON BONE
+        0x1F357,  // POULTRY LEG
+        0x1F358,  // RICE CRACKER
+        0x1F359,  // RICE BALL
+        0x1F35A,  // COOKED RICE
+        0x1F35B,  // CURRY AND RICE
+        0x1F35C,  // STEAMING BOWL
+        0x1F35D,  // SPAGHETTI
+        0x1F35E,  // BREAD
+        0x1F35F,  // FRENCH FRIES
+        0x1F360,  // ROASTED SWEET POTATO
+        0x1F361,  // DANGO
+        0x1F362,  // ODEN
+        0x1F363,  // SUSHI
+        0x1F364,  // FRIED SHRIMP
+        0x1F365,  // FISH CAKE WITH SWIRL DESIGN
+        0x1F366,  // SOFT ICE CREAM
+        0x1F367,  // SHAVED ICE
+        0x1F368,  // ICE CREAM
+        0x1F369,  // DOUGHNUT
+        0x1F36A,  // COOKIE
+        0x1F36B,  // CHOCOLATE BAR
+        0x1F36C,  // CANDY
+        0x1F36D,  // LOLLIPOP
+        0x1F36E,  // CUSTARD
+        0x1F36F,  // HONEY POT
+        0x1F370,  // SHORTCAKE
+        0x1F371,  // BENTO BOX
+        0x1F372,  // POT OF FOOD
+        0x1F373,  // COOKING
+        0x1F374,  // FORK AND KNIFE
+        0x1F382,  // BIRTHDAY CAKE
+
+        // 기타 4개 추가
+        0x1F980,  // CRAB
+        0x1F990,  // SHRIMP
+        0x1F99E,  // LOBSTER
+        0x1F41F
+    )
+}
