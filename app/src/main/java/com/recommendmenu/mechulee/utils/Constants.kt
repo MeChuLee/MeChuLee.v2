6765d1235c832c6258e4a273116c89abc1b406ea
JungWooGeon <jwk1734@naver.com>
2023-10-02 15:55:59 +0900

    라이센스 화면 완료 - 이미지와 오픈소스 화면 하나로 수정 + recyclerview + adapter 반영, 버전 표기 화면 삭제

--- app/src/main/java/com/recommendmenu/mechulee/utils/Constants.kt
+++ app/src/main/java/com/recommendmenu/mechulee/utils/Constants.kt
@@ -8,6 +8,68 @@ object Constants {
     const val URL_TYPE_INGREDIENT = 1
 
     const val INTENT_NAME_WEB_URL = "webUrl"
-
     const val INTENT_NAME_RESULT = "object"
+
+    const val INTENT_NAME_LICENSE = "license"
+    const val INTENT_VALUE_LICENSE_IMAGE = "이미지 라이센스"
+    const val INTENT_VALUE_LICENSE_OPENSOURCE = "오픈소스 라이센스"
+
+
+    val OPENSOURCE_LICENSE_LIST = arrayListOf(
+        "lottie",
+        "circleimageview",
+        "expandablebottombar",
+        "magic-goop:tag-sphere"
+    )
+
+    val IMAGE_LICENSE_LIST = arrayListOf(
+        "상추 아이콘 제작자: Freepik - Flaticon",
+        "고기 아이콘 제작자: DinosoftLabs - Flaticon",
+        "버섯 아이콘 제작자: popo2021 - Flaticon",
+        "양고기 아이콘 제작자: Freepik - Flaticon",
+        "케첩 아이콘 제작자: Freepik - Flaticon",
+        "소시지 아이콘 제작자: Freepik - Flaticon",
+        "젤리 아이콘 제작자: Freepik - Flaticon",
+        "고기 아이콘 제작자: Good Ware - Flaticon",
+        "우유 아이콘 제작자: Freepik - Flaticon",
+        "양배추 아이콘 제작자: dDara - Flaticon",
+        "과일 아이콘 제작자: Freepik - Flaticon",
+        "떡 아이콘 제작자: Freepik - Flaticon",
+        "팥 아이콘 제작자: justicon - Flaticon",
+        "새우 아이콘 제작자: Freepik - Flaticon",
+        "오리 아이콘 제작자: Freepik - Flaticon",
+        "쉘 링크 아이콘 제작자: Freepik - Flaticon",
+        "김치 아이콘 제작자: Flat Icons - Flaticon",
+        "음식과 식당 아이콘 제작자: Freepik - Flaticon",
+        "카레 아이콘 제작자: Freepik - Flaticon",
+        "오징어 아이콘 제작자: Freepik - Flaticon",
+        "햄 아이콘 제작자: Freepik - Flaticon",
+        "물고기 아이콘 제작자: Freepik - Flaticon",
+        "물고기 아이콘 제작자: surang - Flaticon",
+        "과일 아이콘 제작자: Freepik - Flaticon",
+        "고기 아이콘 제작자: Freepik - Flaticon",
+        "토마토 아이콘 제작자: Rudiyana - Flaticon",
+        "라자냐 아이콘 제작자: Freepik - Flaticon",
+        "고구마 아이콘 제작자: Mihimihi - Flaticon",
+        "쌀 아이콘 제작자: Freepik - Flaticon",
+        "중국말 아이콘 제작자: ultimatearm - Flaticon",
+        "멸치 아이콘 제작자: Freepik - Flaticon",
+        "Anise icons created by Smashicons - Flaticon",
+        "두부 아이콘 제작자: Freepik - Flaticon",
+        "호박 아이콘 제작자: popo2021 - Flaticon",
+        "돼지 아이콘 제작자: iconixarPro - Flaticon",
+        "야채 아이콘 제작자: max.icons - Flaticon",
+        "Seaweed icons created by Kise1ki - Flaticon",
+        "소스 아이콘 제작자: Ina Mella - Flaticon",
+        "소고기 아이콘 제작자: flatart_icons - Flaticon",
+        "파프리카 아이콘 제작자: Sophia tkx - Flaticon",
+        "메밀 아이콘 제작자: Freepik - Flaticon",
+        "Octopus icons created by Amethyst prime - Flaticon",
+        "해초 아이콘 제작자: dDara - Flaticon",
+        "고등어 아이콘 제작자: surang - Flaticon",
+        "우동 아이콘 제작자: Freepik - Flaticon",
+        "갈비 살 아이콘 제작자: kerismaker - Flaticon",
+        "인삼 아이콘 제작자: surang - Flaticon",
+        "한국어 아이콘 제작자: surang - Flaticon"
+    )
 }
